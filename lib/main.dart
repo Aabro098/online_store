25d6e8b506da6f469931d9bf6bb5d226fe093c6f
Aabro098 <arbinstha71@gmail.com>
2024-07-13 20:55:54 +0545

    project set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:online_store/Constants/global_variables.dart';
+import 'Features/Screens/Auth Screen/Sign In/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,8 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp(
+      title: 'Online Store',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor
+        )
+      ),
+      home: const SignInScreen(),
     );
   }
 }
